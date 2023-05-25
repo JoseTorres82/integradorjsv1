584d354c3fa6a73f6fbe9e61ef0d72d0cce008a0
JoseTorres82 <josejtorres82@gmail.com>
2023-05-25 07:28:30 -0300

    integradodJs_NoResponsiveNoJs

---
+++ assets/js/main.js
@@ -0,0 +1,89 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "",
+      precio: 75.000,
+      caracteristicas: ["Memoria", "Almacenamiento", "Pantalla"],
+      imagen: "",
+    },
+    {
+      id: 2,
+      nombre: "",
+      precio: 75.000,
+      caracteristicas: ["Memoria", "Almacenamiento", "Pantalla"],
+      imagen: "",
+    },
+    {
+      id: 3,
+      nombre: "",
+      precio: 75.000,
+      caracteristicas: ["Memoria", "Almacenamiento", "Pantalla"],
+      imagen: "",
+    },
+    {
+      id: 4,
+      nombre: "",
+      precio: 75.000,
+      caracteristicas: ["Memoria", "Almacenamiento", "Pantalla"],
+      imagen: "",
+    },
+    {
+      id: 5,
+      nombre: "",
+      precio: 75.000,
+      caracteristicas: ["Memoria", "Almacenamiento", "Pantalla"],
+      imagen: "",
+    },
+    {
+      id: 6,
+      nombre: "",
+      precio: 75.000,
+      caracteristicas: ["Memoria", "Almacenamiento", "Pantalla"],
+      imagen: "",
+    },
+    {
+      id: 7,
+      nombre: "",
+      precio: 75.000,
+      caracteristicas: ["Memoria", "Almacenamiento", "Pantalla"],
+      imagen: "",
+    },
+    {
+      id: 8,
+      nombre: "",
+      precio: 75.000,
+      caracteristicas: ["Memoria", "Almacenamiento", "Pantalla"],
+      imagen: "",
+    },
+    {
+      id: 9,
+      nombre: "",
+      precio: 75.000,
+      caracteristicas: ["Memoria", "Almacenamiento", "Pantalla"],
+      imagen: "",
+    },
+    {
+      id: 10,
+      nombre: "",
+      precio: 75.000,
+      caracteristicas: ["Memoria", "Almacenamiento", "Pantalla"],
+      imagen: "",
+    },
+    {
+      id: 11,
+      nombre: "",
+      precio: 75.000,
+      caracteristicas: ["Memoria", "Almacenamiento", "Pantalla"],
+      imagen: "",
+    },
+    {
+      id: 12,
+      nombre: "",
+      precio: 75.000,
+      caracteristicas: ["Memoria", "Almacenamiento", "Pantalla"],
+      imagen: "",
+    },
+  
+    
+  ];
+  
